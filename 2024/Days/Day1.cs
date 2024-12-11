1e2eec5801a589d24f9622030f07cbc5523b6988
beauchama <beauchama@pulsewave.dev>
2024-12-10 21:54:04 -0500

    Day 1

---
+++ 2024/Days/Day1.cs
@@ -0,0 +1,43 @@
+﻿namespace Advent.Days;
+
+internal class Day1
+{
+    private readonly (int left, int right)[] _inputs = ReadInputs();
+
+    public int SolvePartOne()
+    {
+        return _inputs.Sum(i => Math.Abs(i.left - i.right));
+    }
+
+    public int SolvePartTwo()
+    {
+        IEnumerable<int> lefts = _inputs.Select(i => i.left);
+
+        int score = 0;
+        foreach(int left in lefts)
+        {
+            score += left * _inputs.Count(i => i.right == left);
+        }
+
+        return score;
+    }
+
+    private static (int left, int right)[] ReadInputs()
+    {
+        ReadOnlySpan<string> inputs = File.ReadAllLines("Inputs/input-day1.txt");
+        Span<Range> ranges = stackalloc Range[2];
+
+        List<int> left = new(inputs.Length);
+        List<int> right = new(inputs.Length);
+
+        foreach (ReadOnlySpan<char> input in inputs)
+        {
+            _ = input.Split(ranges, ' ', StringSplitOptions.RemoveEmptyEntries);
+
+            left.Add(int.Parse(input[ranges[0]]));
+            right.Add(int.Parse(input[ranges[1]]));
+        }
+
+        return [.. left.Order().Zip(right.Order())];
+    }
+}
---
+++ 2024/Program.cs
@@ -0,0 +1,7 @@
+﻿using Advent.Days;
+
+Console.WriteLine("Advent of Code 2024\r\n");
+
+Day1 day1 = new();
+Console.WriteLine($"Day 1 - Part 1: {day1.SolvePartOne()}");
+Console.WriteLine($"Day 1 - Part 2: {day1.SolvePartTwo()}");
