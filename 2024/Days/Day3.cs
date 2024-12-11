f018d7ac2696815566177a2dba2f5cc0fc956652
beauchama <beauchama@pulsewave.dev>
2024-12-11 00:15:22 -0500

    Day 3 - part 1

---
+++ 2024/Days/Day3.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent.Days;
+
+internal sealed partial class Day3
+{
+    private readonly string _input = File.ReadAllText("inputs/day3.txt");
+
+    [GeneratedRegex(@"mul\((?<left>\d{1,3}),(?<right>\d{1,3})\)")]
+    private static partial Regex MultiplyRegex { get; }
+
+    public int SolvePartOne()
+    {
+        int result = 0;
+        foreach(Match match in MultiplyRegex.Matches(_input))
+        {
+            int left = int.Parse(match.Groups["left"].Value);
+            int right = int.Parse(match.Groups["right"].Value);
+
+            result += left * right;
+        }
+
+        return result;
+    }
+}
--- 2024/Program.cs
+++ 2024/Program.cs
@@ -2,10 +2,17 @@
 
 Console.WriteLine("Advent of Code 2024\r\n");
 
-//Day1 day1 = new();
-//Console.WriteLine($"Day 1 - Part 1: {day1.SolvePartOne()}");
-//Console.WriteLine($"Day 1 - Part 2: {day1.SolvePartTwo()}");
+Day1 day1 = new();
+Console.WriteLine("Day 1");
+Console.WriteLine($"Part 1: {day1.SolvePartOne()}");
+Console.WriteLine($"Part 2: {day1.SolvePartTwo()}\r\n");
 
 Day2 day2 = new();
-Console.WriteLine($"Day 2 - Part 1: {day2.SolvePartOne()}");
-Console.WriteLine($"Day 2 - Part 2: {day2.SolvePartTwo()}");
+Console.WriteLine("Day 2");
+Console.WriteLine($"Part 1: {day2.SolvePartOne()}");
+Console.WriteLine($"Part 2: {day2.SolvePartTwo()}\r\n");
+
+Day3 day3 = new();
+Console.WriteLine("Day 3");
+Console.WriteLine($"Part 1: {day3.SolvePartOne()}");
+//Console.WriteLine($"Part 2: {day3.SolvePartTwo()}\r\n");
